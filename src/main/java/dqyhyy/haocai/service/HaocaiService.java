861ad28cfeea1f0ff40e6dcd1c326e47e4fe41a8
Zhe Wang <zhew1118@gmail.com>
2018-11-08 15:37:54 +0800

    Create service package, separate service and its implementation, implement four basic methods

---
+++ src/main/java/dqyhyy/haocai/service/HaocaiService.java
@@ -0,0 +1,42 @@
+package dqyhyy.haocai.service;
+
+import dqyhyy.haocai.domain.Haocai;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface HaocaiService {
+    /**
+     * 根据耗材id查找一种耗材
+     * @param haocaiId
+     * @return
+     */
+    Haocai findOne(Integer haocaiId);
+
+
+    /**
+     * 查询所有上架耗材
+     * @return
+     */
+    List<Haocai> findUpAll();
+
+
+    /**
+     * 分页查询所有耗材
+     * @param pageable
+     * @return
+     */
+    Page<Haocai> findAll(Pageable pageable);
+
+
+    /**
+     * 存储一种耗材
+     * @param haocai
+     * @return
+     */
+    Haocai save(Haocai haocai);
+
+
+
+}
---
+++ src/main/java/dqyhyy/haocai/service/impl/haocaiServiceImpl.java
@@ -0,0 +1,62 @@
+package dqyhyy.haocai.service.impl;
+
+import dqyhyy.haocai.domain.Haocai;
+import dqyhyy.haocai.enums.HaocaiStatusEnum;
+import dqyhyy.haocai.repository.HaocaiRepository;
+import dqyhyy.haocai.service.HaocaiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class haocaiServiceImpl implements HaocaiService {
+
+    @Autowired
+    private HaocaiRepository haocaiRepository;
+
+    /**
+     * 根据耗材id查找一种耗材
+     *
+     * @param haocaiId
+     * @return
+     */
+    @Override
+    public Haocai findOne(Integer haocaiId) {
+        return haocaiRepository.findById(haocaiId).get();
+    }
+
+    /**
+     * 查询所有上架耗材， 1上架， 0下架， 详见HaocaiStatusEnum.java
+     *
+     * @return
+     */
+    @Override
+    public List<Haocai> findUpAll() {
+        return haocaiRepository.findByHaocaiStatus(HaocaiStatusEnum.UP.getCode());
+    }
+
+    /**
+     * 分页查询所有耗材
+     *
+     * @param pageable
+     * @return
+     */
+    @Override
+    public Page<Haocai> findAll(Pageable pageable) {
+        return haocaiRepository.findAll(pageable);
+    }
+
+    /**
+     * 存储一种耗材
+     *
+     * @param haocai
+     * @return
+     */
+    @Override
+    public Haocai save(Haocai haocai) {
+        return haocaiRepository.save(haocai);
+    }
+}
