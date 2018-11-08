bc3c889a0ae39628207c91fa0ceed8a203e9fac7
Zhe Wang <zhew1118@gmail.com>
2018-11-08 15:39:19 +0800

    Add a DAO for filter by Haocai Status

--- src/main/java/dqyhyy/haocai/repository/HaocaiRepository.java
+++ src/main/java/dqyhyy/haocai/repository/HaocaiRepository.java
@@ -3,5 +3,8 @@ package dqyhyy.haocai.repository;
 import dqyhyy.haocai.domain.Haocai;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HaocaiRepository extends JpaRepository<Haocai, Integer> {
+    List<Haocai> findByHaocaiStatus(Integer haocaiStatus);
 }
