fc06132c1353dfdfdcd50a2d15d67e53ee95216b
Zhe Wang <zhew1118@gmail.com>
2018-11-19 21:41:56 +0800

    Create Haocai form page

---
+++ src/main/java/dqyhyy/haocai/form/HaocaiForm.java
@@ -0,0 +1,30 @@
+package dqyhyy.haocai.form;
+
+public class HaocaiForm {
+
+    private String haocaiId;
+
+    /** 耗材名称 */
+    private String haocaiName;
+
+    /** 耗材购买时间 */
+    private String haocaiPurchaseDate;
+
+    /** 耗材有效期 */
+    private String haocaiExpirationDate;
+
+    /** 耗材规格 */
+    private String haocaiSpec;
+
+    /** 耗材供应商 */
+    private String haocaiVendor;
+
+    /** 耗材种类 */
+    private String haocaiType;
+
+    /** 耗材库存余量 */
+    private int haocaiAmount;
+
+    /** 耗材库存单位 */
+    private String haocaiUnit;
+}
