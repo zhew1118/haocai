f8301cff4ebb9a0e1f8b9a2d1b8dba2b045a2b70
Zhe Wang <zhew1118@gmail.com>
2018-11-20 15:53:33 +0800

    Add lombok dependency

--- src/test/java/dqyhyy/haocai/service/impl/haocaiServiceImplTest.java
+++ src/test/java/dqyhyy/haocai/service/impl/haocaiServiceImplTest.java
@@ -1,6 +1,8 @@
 package dqyhyy.haocai.service.impl;
 
 import dqyhyy.haocai.domain.Haocai;
+import dqyhyy.haocai.enums.HaocaiStatusEnum;
+import lombok.Data;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,14 +16,15 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Data
 public class haocaiServiceImplTest {
     @Autowired
     private haocaiServiceImpl haocaiService;
 
     @Test
     public void findOne() {
-        Haocai haocai = haocaiService.findOne(1);
-        Assert.assertEquals(1, (int)haocai.getHaocaiId());
+        Haocai haocai = haocaiService.findOne("1");
+        Assert.assertEquals("1", haocai.getHaocaiId());
     }
 
     @Test
@@ -40,10 +43,21 @@ public class haocaiServiceImplTest {
     @Test
     public void save() {
         Haocai haocai = new Haocai();
+        haocai.setHaocaiId("10");
         haocai.setHaocaiName("针筒");
         haocai.setHaocaiAmount(2000);
+        haocai.setHaocaiExpirationDate("20191111");
+        haocai.setHaocaiPurchaseDate("20181111");
+        haocai.setHaocaiStatus(HaocaiStatusEnum.DOWN.getCode());
+        haocai.setHaocaiSpec("小型");
 
         Haocai result = haocaiService.save(haocai);
         Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void onSale(){
+        Haocai result = haocaiService.onSale("10");
+        Assert.assertEquals(HaocaiStatusEnum.UP, result.getHaocaiStatusEnum());
     }
 }
